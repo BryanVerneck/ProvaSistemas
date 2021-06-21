557e2703b0fe0b576546cf02a17cdd3f418ab719
Bryan Verneck <bryanverneck@gmail.com>
2021-06-21 20:02:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ date_default_timezone_set( 'America/São_Paulo');
 
 $dataAgora = date(DATE_RFC822);
 $nomeUsuario = "Bryan Verneck";
+$nomeAtividade = "Avalia&ccedil;&atilde;o SOCPS II";
 
+echo "Atividade: {$nomeAtividade} <br>";
 echo "Nome Estudande: {$nomeUsuario} <br>";
 echo "Data e hora carregamento: {$dataAgora} <br>";
 
